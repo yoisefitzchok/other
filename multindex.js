f3aaf532911ba3fc4dace1ae015218af471c3142
yoisef yitzchok rottenberg <51050625+yoisefitzchok@users.noreply.github.com>
2023-06-06 01:40:03 +0300

    שיפור על פי מקור חיצוני

--- multindex.js
+++ multindex.js
@@ -1,12 +1,4 @@
 function match_index(stringORarray, search_value) {
-var result = [], number = 0;
-
-do {
-number = stringORarray.indexOf(search_value, number);
-result.push(number++);
-}
-while (number > 0);
-
-result.pop();
-return result;
-}
+var result = [], number = -1;
+while ((number = stringORarray.indexOf(search_value, number+1)) >= 0) result.push(number++);
+return result}
